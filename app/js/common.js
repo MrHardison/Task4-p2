b131fc1ab05bab0702d4240577cad7aa5c1f3811
Dmitriy Nosovskiy <disscovery.993@gmail.com>
2018-09-27 14:14:45 +0300

    upd

---
+++ app/js/common.js
@@ -0,0 +1,26 @@
+$(function() {
+	var swiper = new Swiper('.swiper-container', {
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		  renderBullet: function (index, className) {
+			return '<li class="' + className + '">' + (index + 1) + '</li>';
+		  },
+		},
+		});
+
+		var hamburger = $('.js-toggle-menu');
+		var menu = $('.header-menu');
+		
+		hamburger.on('click', function() {
+			hamburger.toggleClass('hamburger-block_active');
+			menu.toggleClass('header-menu_open');
+		});
+
+		$(window).resize(function() {
+			if($(window).width() >= 960) {
+				hamburger.removeClass('hamburger-block_active');
+				menu.removeClass('header-menu_open');
+			}
+ 		});
+});
